f45912f14dc940847c60e5932fcfe55804c145e2
Kyle Knoepfel <knoepfel@fnal.gov>
2024-05-14 15:59:24 -0500

    Massive recipe cleanup using FnalGithubPackage

--- packages/larbatch/package.py
+++ packages/larbatch/package.py
@@ -4,27 +4,21 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
+from spack.pkg.fnal_art.fnal_github_package import FnalGithubPackage
 
-class Larbatch(CMakePackage):
+
+class Larbatch(CMakePackage, FnalGithubPackage):
     """package for batch job submission featuring project.py"""
 
-    homepage = "https://cdcvs.fnal.gov/redmine/projects/larbatch-web-client/wiki"
-    url = "https://github.com/LArSoft/larbatch/archive/refs/tags/v01_51_15.tar.gz"
+    repo = "LArSoft/larbatch"
+    version_patterns = ["v01_00_00"]
 
     version("01.59.04", sha256="8158d2e1b5f208d1014b3745c347d49e3e3449c240842af5361c67fd3b269dff")
-    version("1.51.15", sha256="adc956e621f36c7fbf37f85c737e793d8fc8e58ad44ec3077ea80830f1b7ad25")
+    version("develop", branch="develop", get_full_repo=True)
 
     depends_on("sam-web-client", type=("run"))
     depends_on("python", type=("run"))
-    depends_on("cetmodules", type="build") 
-
-    def url_for_version(self, version):
-        urlf = "https://github.com/LArSoft/larbatch/archive/refs/tags/v%s.tar.gz"
-        return urlf % version.underscored
-
-    version("1.51.15", sha256="adc956e621f36c7fbf37f85c737e793d8fc8e58ad44ec3077ea80830f1b7ad25")
-
-    patch("cmake.patch" , when=("@:1.57.00")) 
+    depends_on("cetmodules", type="build")
 
     @run_before("cmake")
     def patch_version(self):
@@ -32,16 +26,14 @@ class Larbatch(CMakePackage):
         filter_file(
             r"project\(larbatch .*\)",
             "project(larbatch VERSION {})".format(self.spec.version),
-            "CMakeLists.txt"
+            "CMakeLists.txt",
         )
 
-    def cmake_args(self):
-        args = [
-        ]
-        return args
-
-    def setup_run_environment(self, run_env):
-        run_env.prepend_path("PATH", self.prefix.bin)
-        run_env.prepend_path("PYTHONPATH", self.prefix.bin)
-        run_env.prepend_path("PYTHONPATH", self.prefix.python)
-        run_env.prepend_path("PYTHONPATH", str(self.prefix.larbatch.v) + str(self.spec.version.underscored) + "/python")
+    def setup_run_environment(self, env):
+        env.prepend_path("PATH", self.prefix.bin)
+        env.prepend_path("PYTHONPATH", self.prefix.bin)
+        env.prepend_path("PYTHONPATH", self.prefix.python)
+        env.prepend_path(
+            "PYTHONPATH",
+            str(self.prefix.larbatch.v) + str(self.spec.version.underscored) + "/python",
+        )
