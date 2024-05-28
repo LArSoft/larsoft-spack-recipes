27935ee7b9234537c0bebabb624cd2cdddedfb25
Patrick Gartung <gartung@fnal.gov>
2024-05-28 14:55:49 -0500

    Remove torch-scatter from larrecodnn

--- packages/larrecodnn/package.py
+++ packages/larrecodnn/package.py
@@ -46,7 +46,6 @@ class Larrecodnn(CMakePackage, FnalGithubPackage):
     depends_on("py-tensorflow")
     depends_on("py-torch")
     depends_on("py-torch-scatter")
-    depends_on("torch-scatter")
     depends_on("root")
     depends_on("tbb")
     depends_on("triton")
