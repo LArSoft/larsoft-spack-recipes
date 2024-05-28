61ad39146ca82ca468007c45400d9b6ce645938c
Patrick Gartung <gartung@fnal.gov>
2024-05-28 15:41:29 -0500

    Copy py-torch-scatter with cmake build to torch-scatter

--- packages/torch-scatter/package.py
+++ packages/torch-scatter/package.py
@@ -3,35 +3,62 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install torch-scatter
-#
-# You can edit this file again by typing:
-#
-#     spack edit torch-scatter
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class TorchScatter(CMakePackage):
+    """PyTorch Extension Library of Optimized Scatter Operations."""
 
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rusty1s/pytorch_scatter"
     url = "https://github.com/rusty1s/pytorch_scatter/archive/refs/tags/2.1.2.tar.gz"
+    git = "https://github.com/rusty1s/pytorch_scatter.git"
+
+    license("MIT")
+    maintainers("adamjstewart")
 
     version("2.1.2", sha256="6f375dbc9cfe03f330aa29ea553e9c7432e9b040d039b041f08bf05df1a8bf37")
 
-    depends_on("py-torch")
+    depends_on("python", type=("build", "link", "run"))
+    depends_on("py-setuptools", type="build")
+
+    # Undocumented dependencies
+    depends_on("py-torch", type=("build", "link", "run"))
+
+    # Historical dependencies
+    depends_on("py-pytest-runner", type="build", when="@:2.0.7")
+
+    variant(
+        "cxxstd",
+        default="17",
+        values=("17", "20"),
+        multi=False,
+        description="Use the specified C++ standard when building.",
+    )
+
+    def setup_build_environment(self, env):
+        if self.spec.satisfies("@2.0.6:"):
+            if "+cuda" in self.spec["py-torch"]:
+                env.set("FORCE_CUDA", 1)
+                env.set("FORCE_ONLY_CUDA", 0)
+                env.set("FORCE_ONLY_CPU", 0)
+            else:
+                env.set("FORCE_CUDA", 0)
+                env.set("FORCE_ONLY_CUDA", 0)
+                env.set("FORCE_ONLY_CPU", 1)
+        else:
+            if "+cuda" in self.spec["py-torch"]:
+                env.set("FORCE_CUDA", 1)
+                env.set("FORCE_CPU", 0)
+            else:
+                env.set("FORCE_CUDA", 0)
+                env.set("FORCE_CPU", 1)
+
+        env.prepend_path("CMAKE_PREFIX_PATH", "{0}".format(self.spec["py-torch"].package.cmake_prefix_paths[0]))
 
     def cmake_args(self):
-        args = []
-        return args
+        define = self.define
+        define_from_variant = self.define_from_variant
+        options = []
+        
+        options.append(define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"))
+        return options
