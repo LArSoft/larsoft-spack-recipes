0e1c08cfd34e9081596a83ab2f2f1059d34427ea
Patrick Gartung <gartung@fnal.gov>
2024-10-11 17:16:47 -0500

    missing :

--- packages/larpandoracontent/package.py
+++ packages/larpandoracontent/package.py
@@ -48,7 +48,7 @@ class Larpandoracontent(CMakePackage, FnalGithubPackage):
         ]
 
     @property
-    def cmake_prefix_paths(self)
+    def cmake_prefix_paths(self):
         return "{0}/lib/python{1}/site-packages/torch".format(
                     self.spec["py-torch"].prefix, self.spec["python"].version.up_to(2)
                 )
