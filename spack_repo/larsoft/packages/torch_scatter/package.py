3a46bc4e56056d8c2c11da3ed18275c9aaf40dba
Patrick Gartung <gartung@fnal.gov>
2025-09-18 17:50:31 -0500

    Update torch_scatter CMAKE_PREFIX_PATH for py-torch

--- spack_repo/larsoft/packages/torch_scatter/package.py
+++ spack_repo/larsoft/packages/torch_scatter/package.py
@@ -55,4 +55,6 @@ class TorchScatter(CMakePackage):
 
     @property
     def cmake_prefix_paths(self):
-        return "{0}".format(self.spec["py-torch"].package.cmake_prefix_paths[0])
+        return "{0}/lib/python{1}/site-packages/torch".format(
+                    self.spec["py-torch"].prefix, self.spec["python"].version.up_to(2)
+                )
