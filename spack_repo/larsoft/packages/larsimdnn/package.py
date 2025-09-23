a8757562f763a25693b27a9b5dec5333a4ca9c05
Patrick Gartung <gartung@fnal.gov>
2025-09-22 20:15:35 -0500

    Set TENSORFLOW_DIR correctly

--- spack_repo/larsoft/packages/larsimdnn/package.py
+++ spack_repo/larsoft/packages/larsimdnn/package.py
@@ -74,7 +74,7 @@ class Larsimdnn(CMakePackage, FnalGithubPackage):
     @when("+tensorflow")
     def setup_build_environment(self, env):
         env.set("TENSORFLOW_DIR", join_path(self.spec["py-tensorflow"].prefix.lib,
-                "python%s/site-packages/tensorflow/include" % self.spec["python"].version.up_to(2),
+                "python%s/site-packages/tensorflow" % self.spec["python"].version.up_to(2),
             ),
         )
         env.set(
