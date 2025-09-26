83724d8edcd0b4fb60574018924c4a5794f6633e
Patrick Gartung <gartung@fnal.gov>
2025-09-26 11:05:51 -0500

    Add larfinder latest version

--- spack_repo/larsoft/packages/larfinder/package.py
+++ spack_repo/larsoft/packages/larfinder/package.py
@@ -17,6 +17,7 @@ class Larfinder(CMakePackage, FnalGithubPackage):
     version_patterns = ["v09_00_01"]
     maintainers = ["marcmengel"]
 
+    version("09.00.04", sha256="fbd2ea2bd04b54e91fcb4b402041f9abdd2d77a09c5a507c1dccc09b1e73221b")
     version("09.00.03", sha256="d6e8b9ee5ca183fe96dc773b0ba727cdc72937f7b26bfdda804a3d0165d6ab00")
     version("09.00.02", sha256="6eaf11a9625832a0e301d46354c06ac420eb4e3a88f26d5d9603eccd1238c380")
     version("09.00.01", sha256="5edbc7eb8a8aa17b4524b72cc1f8ab03691ea730511db0c6167731a9c2e1d659")
