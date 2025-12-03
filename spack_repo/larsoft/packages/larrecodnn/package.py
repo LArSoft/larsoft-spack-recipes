3baa9d8a631cc10c90b9ef184b41575e2dd8b66b
Patrick Gartung <gartung@fnal.gov>
2025-12-03 08:13:24 -0600

    Remove py-torch restiction on larrecodnn

--- spack_repo/larsoft/packages/larrecodnn/package.py
+++ spack_repo/larsoft/packages/larrecodnn/package.py
@@ -69,7 +69,7 @@ class Larrecodnn(CMakePackage, FnalGithubPackage):
     depends_on("nusimdata")
     depends_on("protobuf", when="@:09.23.00")
     depends_on("py-tensorflow", when="+tensorflow")
-    depends_on("py-torch@:2.7")
+    depends_on("py-torch")
     depends_on("torch-scatter")
     depends_on("root")
     depends_on("tbb")
